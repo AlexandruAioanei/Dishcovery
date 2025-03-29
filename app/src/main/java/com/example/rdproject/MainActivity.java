58cbb532ca1fae9899a86d1c2f3963a2e62cc242
Alexandru Aioanei <alex03aioanei@gmail.com>
2025-03-29 13:38:41 +0100

    Project ported from gitlab

---
+++ app/src/main/java/com/example/rdproject/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.rdproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        System.out.println("Daca vedeti asta inseamna ca e bine");
+        System.out.println("Sanatate si numai bine!");
+        System.out.println("ce faci?");
+        System.out.println("Ascultam ploaia!");
+        System.out.println("dar nu ploaua");
+        System.out.println("Nu ploua? Nu cred");
+        System.out.println("Nu bagati de seama culoarea la meniu trebuie facut un collor pallete pt aplicatie");
+
+
+
+
+    }
+}
